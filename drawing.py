bf087b7b9b5653d0a8a86a7baae39c26ceed37ae
Nicholas Pleschko <n.pleschko@gmail.com>
2021-01-29 12:13:38 +0100

    Add time dependant radius

--- drawing.py
+++ drawing.py
@@ -9,12 +9,17 @@ def my_setup():
     stroke(255)
 
 
+time = 0
+
+
 def my_draw():
+    global time
     background(0)
     y = window_height * 0.5
     line((0, y), (window_width, y))
-    circle(window_width*0.5, window_height*0.5, 100)
+    circle(window_width*0.5, window_height*0.5, 100 + sin(time) * 100)
+
+    time = time + 0.01
 
 
-if __name__ == '__main__':
-    run(sketch_setup=my_setup, sketch_draw=my_draw)
+run(sketch_setup=my_setup, sketch_draw=my_draw)
